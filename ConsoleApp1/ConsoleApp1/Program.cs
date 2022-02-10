c9696aebf22b96b172777e76071a79d7f134cfcc
hle66 <taole@ucdavis.edu>
2022-02-10 07:23:35 -0800

    sasas

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+WriteLine("Farewell, World!");
+countCentury();
+int countCentury()
+{
+	ulong years, days, hours, minutes, seconds, milliseconds, nanoseconds, centuries;
+	Console.WriteLine($" input century ");
+
+	centuries = (ulong) Convert.ToInt32(ReadLine());
+
+	years = centuries * 100;
+	
+	days = centuries * 36524;
+	hours = days * 24;
+	minutes = hours * 60;
+	seconds = minutes * 60;
+	milliseconds = seconds * 1000;
+	nanoseconds = milliseconds * 1000000;
+
+	string timedetail = String.Format("{0} centuries = {1} years = {2} days ={3} hours= {4} minutes = {5} seconds= {6} miliseconds = {7} nanoseconds ",
+centuries,years,days,hours,minutes,seconds,milliseconds,nanoseconds);
+
+	WriteLine(timedetail);
+	return 0;
+}
---
+++ Excercise3/Excercise3/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//sfsdfsdfsdfsdfsd
+using static System.Console;
+WriteLine("Hello, World!");
+LowHigh();
+ static void fizzBuzz()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 15 == 0)
+        {
+            Write("FizzBuzz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            Write("Buzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            Write("Fizz\n");
+        }
+        else
+        {
+            Write(i + "\n");
+        }
+
+        if (i % 100 == 0) WriteLine();
+    }
+    WriteLine();
+}
+
+
+static void TestCode()
+{
+  
+    
+        int max = 500;
+        for (byte i = 0; i < max; i++)
+        {
+            WriteLine(i);
+        }
+    
+   
+    
+}
+
+static void LowHigh() {
+    Random r = new Random();
+    int puzzle = r.Next(1, 4);
+    WriteLine("Enter guess");
+    int input = Convert.ToInt32(ReadLine());
+    if (input>0 && input < 4)
+    {
+        if(input > puzzle)
+        {
+            WriteLine("High");
+            return;
+        }
+        else if (input < puzzle)
+        {
+            WriteLine("Low");
+            return;
+        }
+        else
+        {
+            WriteLine("Correct");
+            return;
+        }
+    }
+    else
+    {
+        WriteLine("Out of range");
+        return;
+    }
+
+}
